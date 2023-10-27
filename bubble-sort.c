386ae5e594a271a2815be1ad2ead031a13823e87
Uros Terzic <urosterzicbiz@gmail.com>
2023-10-27 19:00:09 +0200

    Finished with bubble sort algorithm in C

--- bubble-sort.c
+++ bubble-sort.c
@@ -1,3 +1,44 @@
-//
-// Created by Mr Developer on 10/27/23.
-//
+#include "stdio.h"
+#include "stdbool.h"
+
+void swap(int array[], int item1, int item2)
+{
+    int temp = array[item1];
+    array[item1] = array[item2];
+    array[item2] = temp;
+}
+
+void sort(int array[], size_t sizeOfArray)
+{
+    bool isSorted = true;
+    for (int i = 0; i < sizeOfArray; i++)
+    {
+        for (int j = 0; j < sizeOfArray; j++)
+            if (array[j - 1] > array[j]) {
+                swap(array, j, j - 1);
+                isSorted = false;
+        }
+        if (isSorted)
+            break;
+    }
+}
+
+void printArray(int array[], size_t sizeOfArray)
+{
+    for (int i = 0; i < sizeOfArray; i++)
+    {
+        printf("%d", array[i]);
+    }
+}
+
+
+int main()
+{
+    int numbers[10] = { 5, 2, 3, 8, 6, 1, 9, 7, 10, 0};
+    size_t sizeOfArray = sizeof(numbers) / sizeof(int);
+
+    sort(numbers, sizeOfArray);
+
+
+    printArray(numbers, sizeOfArray);
+}
